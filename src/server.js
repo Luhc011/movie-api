49224500121e5f9b1799369f65bd9a244e36772f
lucas <lucashcardoso26@gmail.com>
2023-02-02 14:50:26 -0300

    add migrations files

--- src/server.js
+++ src/server.js
@@ -1,17 +1,16 @@
 require('express-async-errors');
 const AppError = require('./utils/AppError')
+const migrationsRun = require('./database/sqlite/migrations');
+
 const express = require('express'); //import express
 const routes = require('./routes'); //import routes
-const database = require('./database/sqlite');
+
+migrationsRun();
 
 const app = express();
-
-//middleware use
 app.use(express.json());
+
 app.use(routes)
-
-database();
-
 
 app.use((error, request, response, next) => {
 	if (error instanceof AppError) {
